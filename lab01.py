9bc86f7fa4455d1020c46403e613a95ec0e2b8f0
sonpc1 <congson20395@gmail.com>
2020-09-16 10:22:57 +0700

    sonpc1 commit

--- lab01.py
+++ lab01.py
@@ -1,13 +1,11 @@
-ds = []
-for i in range (0,5):
-    ten = 'A' + str(i)
-    tuoi = 25
-    donvi = 'Viettel'
-    temp = [ten, tuoi, donvi]
-    ds.append(temp)
 
+hs1 = {"ten":"A", "tuoi":"25", "donvi": "VIETTEL"}
+hs2 = {"ten":"B", "tuoi":"26", "donvi": "VIETTEL"}    
+hs3 = {"ten":"C", "tuoi":"27", "donvi": "VIETTEL"}
+hs4 = {"ten":"D", "tuoi":"25", "donvi": "VIETTEL"}
+hs5 = {"ten":"E", "tuoi":"25", "donvi": "VIETTEL"}
+ds = [hs1,hs2,hs3,hs4,hs5]
 for row in ds:
-    print('-----------------')
-    for a in row:
-        print(a)
-    
+    print("-----------------------------")
+    for i in row:
+        print(i + ':' + row[i])
